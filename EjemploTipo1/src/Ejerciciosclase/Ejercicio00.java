c3fe18b9444b3caf77f7dd638300e3168f17da9f
Judit Quirós <145467457+jquivio1606@users.noreply.github.com>
2023-09-29 17:34:51 +0200

    Add files via upload

---
+++ EjemploTipo1/src/Ejerciciosclase/Ejercicio00.java
@@ -0,0 +1,19 @@
+package Ejerciciosclase;
+
+import java.util.Scanner;
+
+public class Ejercicio00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Introduce tu nombre, por favor: ");
+		String nombre = scan.nextLine();
+		System.out.println("Hola " + nombre + "!");
+		scan.close(); 
+		
+		
+	}
+
+}
---
+++ EjemploTipo1/src/Ejerciciosclase/PiedraPapelTijera.java
@@ -0,0 +1,22 @@
+package Ejerciciosclase;
+
+public class PiedraPapelTijera {
+
+	public static void main(String[] args) {
+		
+		double num = Math.random()*2;
+		System.out.println("Ha salido "+ (int)num);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
