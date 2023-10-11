1d01927711986d7e215666d9ad029a1a17155464
Judit Quirós <145467457+jquivio1606@users.noreply.github.com>
2023-10-11 17:32:50 +0200

    Add files via upload

---
+++ EjemploTipo1/src/Ejerciciosclase/Ejercicio12.java
@@ -0,0 +1,19 @@
+package Ejerciciosclase;
+
+public class Ejercicio12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char c1 = 'A';
+		char c2 = 'B';
+		char c3 = 'C';
+		char c4 = 'D';
+		char c5 = 'E';
+		String cadena = "" + c1 + c2 + c3 + c4 + c5;
+
+		System.out.println(cadena);
+		
+	}
+
+}
