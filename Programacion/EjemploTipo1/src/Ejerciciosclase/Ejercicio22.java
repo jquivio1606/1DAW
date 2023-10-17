78fdc4df4971e8c1c8244a091ac42b7c588d5169
Judit Quirós <145467457+jquivio1606@users.noreply.github.com>
2023-10-17 17:32:42 +0200

    Add files via upload

---
+++ Programacion/EjemploTipo1/src/Ejerciciosclase/Ejercicio22.java
@@ -0,0 +1,40 @@
+package Ejerciciosclase;
+
+import java.util.Scanner;
+
+public class Ejercicio22 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Introduce un valor para x: ");
+		int x = scan.nextInt();
+		System.out.println("Introduce un valor para y, que sea mayor que x: ");
+		int y = scan.nextInt();
+		String multiplos = "";
+		String result = "Los multiplos de siete entre: " + x + " y " + y + " son: " + multiplos;
+		boolean existen = false;
+		scan.close();
+		
+		
+		if (x < 0 || y < 0 || x > y ) {
+			System.out.println("Los datos introducidos son erróneos. Introdúcelos bien: ");
+		
+		} else {
+			
+			for (int cont = x ; cont <= y ; cont ++) {
+				if (cont % 7 == 0) {
+					multiplos += "\n" + cont;
+					System.out.println(cont);
+					existen = true;
+				}
+					
+			} if (!existen) {
+					result = result + x + " y " + y + " son: " + multiplos;
+			} 
+			System.out.println(result);
+		}
+		
+	}
+
+}
---
+++ Programacion/Tarea2/src/com/JuditQuirosViolero/tarea2/Ejercicio2.java
@@ -0,0 +1,60 @@
+package com.JuditQuirosViolero.tarea2;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+	/*	2. Construye un programa que simule el juego de la adivinanza de un número. 
+		  El ordenador debe generar un número aleatorio entre 1 y 100 y el usuario tiene 
+		  cinco oportunidades para acertarlo. 
+		  Después de cada intento el programa debe indicarle al usuario si el número 
+		  introducido por él es mayor, menor o igual al número a adivinar, y el número de
+		  intentos restantes.
+		 
+		  Nota: para generar el valor aleatorio puede emplearse la sentencia:
+		  int aleatorio = (int) (100*Math.random()+1);											*/
+
+
+		System.out.println("El ordenador generará un número aleatorio y tienes 5 intentos para acertalo.");
+		
+		int aleatorio = (int) (100*Math.random()+1);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int intentos = 5;
+		
+		do {
+		
+			System.out.println("Inserte un número: ");
+			int num = scan.nextInt();
+			
+		if (aleatorio == num) {
+			System.out.println("Has acertado el número elegido por el ordenador era: " + aleatorio);
+			break;
+		} else { 
+			if (num < aleatorio) {
+				intentos--;
+				System.out.println( num + " es más pequeño que el número elegido por el ordenador. Te quedan " + intentos + " intentos restantes");
+				
+			} else {
+				intentos--;
+				System.out.println( num + " es más grande que el número elegido por el ordenador. Te quedan " + intentos + " intentos restantes");
+				
+			}
+		}
+		} while (intentos > 0); 
+		
+		
+		if (intentos == 0) {
+            System.out.println("Agotaste tus intentos. El número a adivinar era: " + aleatorio);
+		}
+		scan.close();
+	}
+}
+			
+		
+	
+
+
