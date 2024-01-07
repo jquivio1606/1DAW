14ff98b2c1b845fed37a3537819f1905e7dc25ea
jquivio1606 <jquivio1606@g.educaand.es>
2024-01-07 21:33:41 +0100

    Avanzando con la tarea

---
+++ Programacion/tarea3CadenasArrays/src/com/JuditQuirosViolero/tarea3/UsoDeStringBuilder.java
@@ -0,0 +1,53 @@
+package com.JuditQuirosViolero.tarea3;
+
+public class UsoDeStringBuilder {
+	public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
+
+/* String prueba = "Hola a todo el mundo"; String prueba2 =
+ * "Hola a todo el mundo menos a uno"; String prueba3 = ""; String prueba4 =
+ * "Adios a todos!!";
+ * 
+ * 
+ * if (prueba3.isEmpty()) { System.out.println("Está vacía"); } else {
+ * System.out.println("Está llena"); }
+ * 
+ * if (prueba2.startsWith("H")) { System.out.println("La cadena empieza por H");
+ * } else { System.out.println("La cadena no empieza por H"); }
+ * 
+ * 
+ * System.out.println("La diferencia entre la cadena 1 y la cadena 2 es:" +
+ * StringUtils.difference(prueba, prueba2));
+ * 
+ * System.out.println(StringUtils.reverse(prueba));
+ * 
+ * StringUtils.countMatches(prueba4, "o"); 
+ * System.out.println("Hay " + StringUtils.countMatches(prueba4, "o") + " en la cadena.");
+ * 
+ * if (StringUtils.isNumeric(prueba3)) {
+ * System.out.println("La cadena es numérica."); } else {
+ * System.out.println("La cadena no es numérica."); }
+ * 
+ * String resultado= StringUtils.defaultIfEmpty(prueba3, "La cadena original está vacía"); 
+ * System.out.println(resultado);
+ */	
